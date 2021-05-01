3f970ff0d8ed70496c697ab9f9ba0f2e2b198d8d
HowardHughes98 <82488909+HowardHughes98@users.noreply.github.com>
2021-05-01 15:33:21 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+var number = +prompt('введите число');
+var degree = +prompt('введит степень');
+var answer = 1;
+var i = 0;
+
+while ( i < degree ) {
+    answer = answer * number;
+    console.log(answer);
+    i++;
+}
+
+
+
+
